36a564c5a9bf277948d9bd0c37a9c650ff83a090
Phạm Nhật Minh <113897170+nhatminh02@users.noreply.github.com>
2024-02-29 19:52:43 +0700

    feat: add exceoption for auth handler

--- capstone-project-be.Application/DTOs/Auths/SignUpVerificationDTO.cs
+++ capstone-project-be.Application/DTOs/Auths/SignUpVerificationDTO.cs
@@ -2,10 +2,7 @@
 {
     public class SignUpVerificationDTO
     {
-        public required string LastName { get; set; }
-        public required string FirstName { get; set; }
         public required string Email { get; set; }
-        public required string Password { get; set; }
         public string VerificationCode { get; set; }
 
     }
--- capstone-project-be.Application/Features/Auths/Handles/ResetPasswordCodeHandler.cs
+++ capstone-project-be.Application/Features/Auths/Handles/ResetPasswordCodeHandler.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using capstone_project_be.Application.DTOs.Users;
+﻿using capstone_project_be.Application.DTOs.Users;
 using capstone_project_be.Application.Features.Auths.Requests;
 using capstone_project_be.Application.Interfaces;
 using capstone_project_be.Application.Responses;
@@ -10,19 +9,37 @@ namespace capstone_project_be.Application.Features.Auths.Handles
     public class ResetPasswordCodeHandler : IRequestHandler<ResetPasswordCodeRequest, object>
     {
         private readonly IUnitOfWork _unitOfWork;
-        private readonly IMapper _mapper;
 
-        public ResetPasswordCodeHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        public ResetPasswordCodeHandler(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
-            _mapper = mapper;
         }
         public async Task<object> Handle(ResetPasswordCodeRequest request, CancellationToken cancellationToken)
         {
             var data = request.ResetPasswordCodeData;
             var userList = await _unitOfWork.UserRepository.Find(user => user.Email == data.Email);
+
+            if(!userList.Any())
+            {
+                return new BaseResponse<UserDTO>()
+                {
+                    IsSuccess = false,
+                    Message = "Không tìm thấy email",
+                };
+            }
+
             var userToUpdate = userList.First();
             var codeList = await _unitOfWork.VerificationCodeRepository.Find(code => code.UserId == userToUpdate.UserId);
+
+            if(!codeList.Any())
+            {
+                return new BaseResponse<UserDTO>()
+                {
+                    IsSuccess = false,
+                    Message = "Mã xác minh không tồn tại",
+                };
+            }
+
             var verificationCode = codeList.First();
 
             if (verificationCode.Code == data.VerificationCode)
