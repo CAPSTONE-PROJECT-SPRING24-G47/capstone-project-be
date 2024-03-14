5198d78adef0f138e99badf549e60a85b0837055
Phuccccccccc <phucphhe161524@fpt.edu.vn>
2024-03-14 23:43:56 +0700

    Feat : Get Blog by UserId

---
+++ capstone-project-be.Application/Features/Blogs/Requests/GetBlogByUserIdRequest.cs
@@ -0,0 +1,11 @@
+﻿using capstone_project_be.Application.DTOs.Blogs;
+using capstone_project_be.Application.Responses;
+using MediatR;
+
+namespace capstone_project_be.Application.Features.Blogs.Requests
+{
+    public class GetBlogByUserIdRequest(string userId) : IRequest<BaseResponse<BlogDTO>>
+    {
+        public string UserId { get; set; } = userId;
+    }
+}
